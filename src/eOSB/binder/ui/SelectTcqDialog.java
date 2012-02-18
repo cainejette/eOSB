d9736275b06baaec33930ae570dd2cce5bd33465
Caine Jette <jette@alum.mit.edu>
2012-02-18 00:29:43 -0800

    Fixed TCQ suggestion logic and removed a question subcategory.

--- src/eOSB/binder/ui/SelectTcqDialog.java
+++ src/eOSB/binder/ui/SelectTcqDialog.java
@@ -40,150 +40,171 @@ import eOSB.score.actions.OpenTcqResultsDialogAction;
  */
 public class SelectTcqDialog extends StandardDialog {
 
-	private JFrame parent; 
+	private JFrame parent;
 	private Handler handler;
 	private Round round;
-	
-  private ButtonGroup buttonGroup = new ButtonGroup();
-  private Map<String, Tcq> map = new HashMap<String, Tcq>();
-  private List<Tcq> availableRounds;
-  private List<JRadioButton> radioButtons;
 
-  /**
-   * @param handler the {@link Handler}
-   */
-  public SelectTcqDialog(Handler handler) {
-  	this.handler = handler;
-  	this.parent = handler.getFrame();
-  	this.round = handler.getCurrentRound();
-  	
-    this.availableRounds = (ArrayList<Tcq>) round.getTcqs();
-    this.availableRounds.addAll((ArrayList<Tcq>) round.getTcqSolutions());
-    this.radioButtons = new ArrayList<JRadioButton>();
+	private ButtonGroup buttonGroup = new ButtonGroup();
+	private Map<String, Tcq> map = new HashMap<String, Tcq>();
+	private List<Tcq> availableRounds;
+	private List<JRadioButton> radioButtons;
+	private JTabbedPane radioButtonsPane;
 
-    JRadioButton button;
-    for (int i = 0; i < availableRounds.size(); i++) {
-      button = availableRounds.get(i).getButton();
-      this.map.put(button.getActionCommand(), availableRounds.get(i));
-      this.radioButtons.add(button);
-      this.buttonGroup.add(button);
-    }
+	/**
+	 * @param handler
+	 *          the {@link Handler}
+	 */
+	public SelectTcqDialog(Handler handler) {
+		this.handler = handler;
+		this.parent = handler.getFrame();
+		this.round = handler.getCurrentRound();
 
-    this.init();
+		this.availableRounds = (ArrayList<Tcq>) round.getTcqs();
+		this.availableRounds.addAll((ArrayList<Tcq>) round.getTcqSolutions());
+		this.radioButtons = new ArrayList<JRadioButton>();
+
+		JRadioButton button;
+		for (int i = 0; i < availableRounds.size(); i++) {
+			button = availableRounds.get(i).getButton();
+			this.map.put(button.getActionCommand(), availableRounds.get(i));
+			this.radioButtons.add(button);
+			this.buttonGroup.add(button);
+		}
+
+		this.init();
+	}
+
+	/**
+	 * Basic dialog initializations.
+	 */
+	private void init() {
+		this.setModal(false);
+		this.pack();
+		this.setTitle("TCQ Selection");
+
+		this.setLocationRelativeTo(this.parent);
+		this.setResizable(false);
+	}
+
+	/**
+	 * Sets the button as selected.
+	 * 
+	 * @param button
+	 *          the button to set as selected.
+	 */
+	public void setSelected(JRadioButton button) {
+  	for (int i = 0; i < radioButtons.size(); i++) {
+  		if (button.equals(radioButtons.get(i))) {
+  			if (i + 1 < radioButtons.size()) {
+					radioButtons.get(i+1).setSelected(true);
+			
+	  			if (i + 1 < 2) {
+	          radioButtonsPane.setSelectedIndex(0);
+	        }
+	        else {
+	          radioButtonsPane.setSelectedIndex(1);
+	        }
+				}
+				else {
+					radioButtons.get(0).setSelected(false);
+					radioButtons.get(1).setSelected(false);
+					radioButtons.get(2).setSelected(false);
+					radioButtons.get(3).setSelected(false);
+					radioButtonsPane.setSelectedIndex(0);
+  			}
+  		}
+  	}
   }
 
-  /**
-   * Basic dialog initializations.
-   */
-  private void init() {
-    this.setModal(false);
-    this.pack();
-    this.setTitle("TCQ Selection");
-    
-    this.setLocationRelativeTo(this.parent);
-    this.setResizable(false);
-  }
+	/** {@inheritDoc} */
+	@Override
+	public JComponent createBannerPanel() {
+		JLabel message = new JLabel("Select a TCQ:");
+		JLabel note = new JLabel(
+				"<HTML>(<i>previously opened</i>, <b>not yet opened</b>)");
+		message.setFont(new Font(message.getFont().getName(), Font.PLAIN, message
+				.getFont().getSize() + 2));
 
-  /**
-   * Sets the button as selected.
-   * @param button the button to set as selected.
-   */
-  public void setSelected(JRadioButton button) {
-//    button.setFont(new Font("Helvetica", Font.ITALIC, 18));
-    int index = this.radioButtons.indexOf(button);
-    if (index < this.radioButtons.size()) {
-      this.radioButtons.get(index).setSelected(true);
-    }
-  }
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(6, 15, 6, 6));
+		panel.setLayout(new MigLayout());
+		panel.add(message, "wrap");
+		panel.add(note);
+		return panel;
+	}
 
-  /** {@inheritDoc} */
-  @Override
-  public JComponent createBannerPanel() {
-  	JLabel message = new JLabel("Select a TCQ:");
-    JLabel note = new JLabel("<HTML>(<i>previously opened</i>, <b>not yet opened</b>)");
-    message.setFont(new Font(message.getFont().getName(), Font.PLAIN, message.getFont().getSize() + 2));
+	/** {@inheritDoc} */
+	@Override
+	public ButtonPanel createButtonPanel() {
+		final ButtonPanel panel = new ButtonPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
 
-    JPanel panel = new JPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(6, 15, 6, 6));
-    panel.setLayout(new MigLayout());
-    panel.add(message, "wrap");
-    panel.add(note);
-    return panel;
-  }
+		OpenTcqAction openTcqAction = new OpenTcqAction(this, this.map,
+				this.buttonGroup);
+		CloseTcqDialogAction cancelAction = new CloseTcqDialogAction(this);
+		this.setDefaultCancelAction(cancelAction);
+		this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
 
-  /** {@inheritDoc} */
-  @Override
-  public ButtonPanel createButtonPanel() {
-    final ButtonPanel panel = new ButtonPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
+		JButton button;
 
-    OpenTcqAction openTcqAction = new OpenTcqAction(this, this.map,
-        this.buttonGroup);
-    CloseTcqDialogAction cancelAction = new CloseTcqDialogAction(this);
-    this.setDefaultCancelAction(cancelAction);
-    this.setDefaultCloseOperation(WindowConstants.DO_NOTHING_ON_CLOSE);
+		button = new JButton();
+		button.setAction(openTcqAction);
+		this.setDefaultAction(openTcqAction);
+		panel.add(button);
 
-    JButton button;
+		OpenTcqResultsDialogAction addTcqResultsAction = new OpenTcqResultsDialogAction(
+				this, this.handler);
+		button = new JButton();
+		button.setAction(addTcqResultsAction);
+		panel.add(button);
+		if (!this.handler.isUsingScoreboard()) {
+			button.setEnabled(false);
+		}
 
-    button = new JButton();
-    button.setAction(openTcqAction);
-    this.setDefaultAction(openTcqAction);
-    panel.add(button);
+		button = new JButton();
+		button.setAction(cancelAction);
+		this.setDefaultCancelAction(cancelAction);
+		button.setText("Close");
+		panel.add(button);
 
-    OpenTcqResultsDialogAction addTcqResultsAction = new OpenTcqResultsDialogAction(this,
-        this.handler);
-    button = new JButton();
-    button.setAction(addTcqResultsAction);
-    panel.add(button);
-    if (!this.handler.isUsingScoreboard()) {
-      button.setEnabled(false);
-    }
+		return panel;
+	}
 
-    button = new JButton();
-    button.setAction(cancelAction);
-    this.setDefaultCancelAction(cancelAction);
-    button.setText("Close");
-    panel.add(button);
-    
-    return panel;
-  }
+	/** {@inheritDoc} */
+	@Override
+	public JComponent createContentPanel() {
 
-  /** {@inheritDoc} */
-  @Override
-  public JComponent createContentPanel() {
+		radioButtonsPane = new JTabbedPane();
+		JPanel questionTab = new JPanel();
+		questionTab.setLayout(new GridLayout(2, 1));
+		questionTab.add(radioButtons.get(0));
+		questionTab.add(radioButtons.get(1));
 
-    JTabbedPane pane = new JTabbedPane();
-    JPanel questionTab = new JPanel();
-    questionTab.setLayout(new GridLayout(2, 1));
-    questionTab.add(radioButtons.get(0));
-    questionTab.add(radioButtons.get(1));
+		JPanel solutionTab = new JPanel();
+		solutionTab.setLayout(new GridLayout(2, 1));
+		solutionTab.add(radioButtons.get(2));
+		solutionTab.add(radioButtons.get(3));
 
-    JPanel solutionTab = new JPanel();
-    solutionTab.setLayout(new GridLayout(2, 1));
-    solutionTab.add(radioButtons.get(2));
-    solutionTab.add(radioButtons.get(3));
+		radioButtonsPane.addTab("Questions", questionTab);
+		radioButtonsPane.addTab("Solutions", solutionTab);
 
-    pane.addTab("Questions", questionTab);
-    pane.addTab("Solutions", solutionTab);
+		// preselect the round and set its tab as open
+		for (int i = 0; i < availableRounds.size(); i++) {
+			if (!availableRounds.get(i).wasPreviouslyOpened()) {
+				radioButtons.get(i).setSelected(true);
+				if (i < 2) {
+					radioButtonsPane.setSelectedIndex(0);
+				} else {
+					radioButtonsPane.setSelectedIndex(1);
+				}
+				break;
+			}
+		}
 
-    // preselect the round and set its tab as open
-    for (int i = 0; i < availableRounds.size(); i++) {
-      if (!availableRounds.get(i).wasPreviouslyOpened()) {
-        radioButtons.get(i).setSelected(true);
-        if (i < 2) {
-          pane.setSelectedIndex(0);
-        }
-        else {
-          pane.setSelectedIndex(1);
-        }
-        break;
-      }
-    }
+		JPanel panel = new JPanel();
+		panel.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
+		panel.add(radioButtonsPane);
 
-    JPanel panel = new JPanel();
-    panel.setBorder(BorderFactory.createEmptyBorder(6, 6, 6, 6));
-    panel.add(pane);
-
-    return panel;
-  }
+		return panel;
+	}
 }
