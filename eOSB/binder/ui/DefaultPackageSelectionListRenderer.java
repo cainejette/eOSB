a9a37e137f5d5c0ad908143e1ec44a439a6ebc02
Caine Jette <caine@attackpattern.com>
2016-03-20 16:44:10 -0700

    Restructuring directories

--- eOSB/binder/ui/DefaultPackageSelectionListRenderer.java
+++ eOSB/binder/ui/DefaultPackageSelectionListRenderer.java
@@ -1,18 +1,22 @@
 package eOSB.binder.ui;
 
+import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.JCheckBox;
+import javax.swing.DefaultListCellRenderer;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.ListCellRenderer;
+import javax.swing.JTextArea;
 import javax.swing.SwingConstants;
 
-@SuppressWarnings("serial")
-public class PackageSelectionListRenderer extends
-		DefaultPackageSelectionListRenderer implements ListCellRenderer {
+public class DefaultPackageSelectionListRenderer extends
+		DefaultListCellRenderer {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
 
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value,
@@ -22,25 +26,22 @@ public class PackageSelectionListRenderer extends
 			JLabel label = (JLabel) value;
 			String name = label.getText();
 			setText(name);
-
+			
 			label.setHorizontalTextPosition(SwingConstants.CENTER);
 			label.setHorizontalAlignment(SwingConstants.CENTER);
 			if (isSelected) {
 				label.setBackground(list.getSelectionBackground());
 				label.setForeground(list.getSelectionForeground());
 			} else {
-				label.setBackground(list.getBackground());
-				label.setForeground(list.getForeground());
+				label.setBackground(Color.LIGHT_GRAY);
+//				label.setForeground(Color.GRAY);
 			}
+			label.setFont(new Font(label.getFont().getName(), Font.BOLD, label.getFont().getSize()));
 
-			label.setFont(new Font(label.getFont().getName(), Font.BOLD, label
-					.getFont().getSize()));
-			label.setEnabled(list.isEnabled());
 			label.setOpaque(true);
 			label.setPreferredSize(new Dimension(200, 35));
 			return label;
-		} 
-		else {
+		} else {
 			return null;
 		}
 	}
--- eOSB/binder/ui/PackageSelectionListRenderer.java
+++ eOSB/binder/ui/PackageSelectionListRenderer.java
@@ -1,18 +1,19 @@
 package eOSB.binder.ui;
 
-import java.awt.Color;
 import java.awt.Component;
 import java.awt.Dimension;
 import java.awt.Font;
 
-import javax.swing.DefaultListCellRenderer;
+import javax.swing.JCheckBox;
 import javax.swing.JLabel;
 import javax.swing.JList;
-import javax.swing.JTextArea;
+import javax.swing.ListCellRenderer;
 import javax.swing.SwingConstants;
 
-public class DefaultPackageSelectionListRenderer extends
-		DefaultListCellRenderer {
+@SuppressWarnings("serial")
+public class PackageSelectionListRenderer extends
+		DefaultPackageSelectionListRenderer implements ListCellRenderer<Object> {
+
 	@Override
 	public Component getListCellRendererComponent(JList list, Object value,
 			int index, boolean isSelected, boolean hasFocus) {
@@ -21,22 +22,25 @@ public class DefaultPackageSelectionListRenderer extends
 			JLabel label = (JLabel) value;
 			String name = label.getText();
 			setText(name);
-			
+
 			label.setHorizontalTextPosition(SwingConstants.CENTER);
 			label.setHorizontalAlignment(SwingConstants.CENTER);
 			if (isSelected) {
 				label.setBackground(list.getSelectionBackground());
 				label.setForeground(list.getSelectionForeground());
 			} else {
-				label.setBackground(Color.LIGHT_GRAY);
-//				label.setForeground(Color.GRAY);
+				label.setBackground(list.getBackground());
+				label.setForeground(list.getForeground());
 			}
-			label.setFont(new Font(label.getFont().getName(), Font.BOLD, label.getFont().getSize()));
 
+			label.setFont(new Font(label.getFont().getName(), Font.BOLD, label
+					.getFont().getSize()));
+			label.setEnabled(list.isEnabled());
 			label.setOpaque(true);
 			label.setPreferredSize(new Dimension(200, 35));
 			return label;
-		} else {
+		} 
+		else {
 			return null;
 		}
 	}
