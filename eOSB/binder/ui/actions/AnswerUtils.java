62afe03a71f1db590ffb6cbb3aa80fa0726a563f
Caine Jette <caine@attackpattern.com>
2016-03-24 22:48:30 -0700

    Simplify AnswerUtils with some ternary operations

--- eOSB/binder/ui/actions/AnswerUtils.java
+++ eOSB/binder/ui/actions/AnswerUtils.java
@@ -16,78 +16,39 @@ public class AnswerUtils {
 			processTossup(question, teamA, teamB);
 		} else {
 			processBonus(question, teamA, teamB);
-
 		}
 	}
 
 	private static void processTossup(Question question, ButtonState teamA, ButtonState teamB) {
-		BuzzerTurn turn;
-		boolean enableTeamA = false;
-		boolean enableTeamB = false;
-
-		if (teamA.isCorrect()) {
-			if (teamB.isInterrupt()) {
-				turn = new BuzzerTurn(BuzzerScoreResult.CORRECT_TOSSUP, BuzzerScoreResult.INTERRUPT);
-			} else {
-				turn = new BuzzerTurn(BuzzerScoreResult.CORRECT_TOSSUP, BuzzerScoreResult.INCORRECT);
-			}
-
-			enableTeamA = true;
-		} else if (teamB.isCorrect()) {
-			if (teamA.isInterrupt()) {
-				turn = new BuzzerTurn(BuzzerScoreResult.INTERRUPT, BuzzerScoreResult.CORRECT_TOSSUP);
-			} else {
-				turn = new BuzzerTurn(BuzzerScoreResult.INCORRECT, BuzzerScoreResult.CORRECT_TOSSUP);
-			}
-
-			enableTeamB = true;
-		} else {
-			if (teamA.isInterrupt()) {
-				if (teamB.isInterrupt()) {
-					turn = new BuzzerTurn(BuzzerScoreResult.INTERRUPT, BuzzerScoreResult.INTERRUPT);
-				} else {
-					turn = new BuzzerTurn(BuzzerScoreResult.INTERRUPT, BuzzerScoreResult.INCORRECT);
-				}
-			} else if (teamB.isInterrupt()) {
-				turn = new BuzzerTurn(BuzzerScoreResult.INCORRECT, BuzzerScoreResult.INTERRUPT);
-			} else {
-				turn = new BuzzerTurn(BuzzerScoreResult.INCORRECT, BuzzerScoreResult.INCORRECT);
-			}
-		}
-
+		BuzzerScoreResult teamAResult = teamA.isCorrect() ? 
+				BuzzerScoreResult.CORRECT_TOSSUP : teamA.isInterrupt() ? 
+							BuzzerScoreResult.INTERRUPT : BuzzerScoreResult.INCORRECT;
+		
+		BuzzerScoreResult teamBResult = teamB.isCorrect() ? 
+				BuzzerScoreResult.CORRECT_TOSSUP : teamB.isInterrupt() ? 
+							BuzzerScoreResult.INTERRUPT : BuzzerScoreResult.INCORRECT;
+	
+		BuzzerTurn turn = new BuzzerTurn(teamAResult, teamBResult);
 		turn.setQuestionNumber(question.getNumber());
+		EventBus.publish(new NextQuestionEvent("AnswerUtils", teamA.isCorrect(), teamB.isCorrect(), turn));
 
-		// if both teams are incorrect on a tossup, move on to next tossup
+		// if both teams were incorrect on a tossup, they don't get the bonus question
 		if (!teamA.isCorrect() && !teamB.isCorrect()) {
-			EventBus.publish(new NextQuestionEvent("AnswerUtils", false, false, turn));
-
-			// add a dummy turn to the match's history so "Back" button can
-			// just remove most recent turn
 			BuzzerTurn spacerTurn = new BuzzerTurn(BuzzerScoreResult.INCORRECT, BuzzerScoreResult.INCORRECT);
 			spacerTurn.setQuestionNumber(question.getNumber());
-			// System.out.println("add turn event from answerUtils");
-			// EventBus.publish(new AddTurnEvent(spacerTurn));
-
 			EventBus.publish(new NextQuestionEvent("AnswerUtils", true, true, spacerTurn));
-		}
-		// one team answered the tossup correctly, so move on to bonus and set
-		// buttons appropriately
-		else {
-			EventBus.publish(new NextQuestionEvent("AnswerUtils", enableTeamA, enableTeamB, turn));
 		}
 	}
 
 	private static void processBonus(Question question, ButtonState teamA, ButtonState teamB) {
-		BuzzerTurn turn;
+		
+		BuzzerScoreResult teamAResult = teamA.isCorrect() ? 
+				BuzzerScoreResult.CORRECT_BONUS : BuzzerScoreResult.INCORRECT;
+		
+		BuzzerScoreResult teamBResult = teamB.isCorrect() ? 
+				BuzzerScoreResult.CORRECT_BONUS : BuzzerScoreResult.INCORRECT;
 
-		if (teamA.isCorrect()) {
-			turn = new BuzzerTurn(BuzzerScoreResult.CORRECT_BONUS, BuzzerScoreResult.INCORRECT);
-		} else if (teamB.isCorrect()) {
-			turn = new BuzzerTurn(BuzzerScoreResult.INCORRECT, BuzzerScoreResult.CORRECT_BONUS);
-		} else {
-			turn = new BuzzerTurn(BuzzerScoreResult.INCORRECT, BuzzerScoreResult.INCORRECT);
-		}
-
+		BuzzerTurn turn = new BuzzerTurn(teamAResult, teamBResult);
 		turn.setQuestionNumber(question.getNumber());
 
 		EventBus.publish(new NextQuestionEvent("AnswerUtils", true, true, turn));
