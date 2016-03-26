a811f023c5b7d7465c96731f177672f7d35d8d9b
Caine Jette <caine@attackpattern.com>
2016-03-25 20:53:30 -0700

    Revamp Edit Time Dialog

--- eOSB/time/ui/EditTimeDialog.java
+++ eOSB/time/ui/EditTimeDialog.java
@@ -1,8 +1,11 @@
 package eOSB.time.ui;
 
 import java.awt.Font;
+import java.text.NumberFormat;
 
+import javax.swing.AbstractAction;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JComponent;
 import javax.swing.JLabel;
@@ -13,6 +16,8 @@ import com.jidesoft.dialog.ButtonPanel;
 import com.jidesoft.dialog.StandardDialog;
 
 import eOSB.binder.ui.actions.CancelButtonAction;
+import eOSB.game.data.IconFactory;
+import eOSB.score.ui.ImprovedFormattedTextField;
 import eOSB.time.actions.SetTimeAction;
 import eOSB.time.controller.CountDownTimer;
 import net.miginfocom.swing.MigLayout;
@@ -20,12 +25,19 @@ import net.miginfocom.swing.MigLayout;
 public class EditTimeDialog extends StandardDialog {
 
 	private CountDownTimer timer;
-	private JTextField minutesField = new JTextField(2);
-	private JTextField secondsField = new JTextField(2);
+	private ImprovedFormattedTextField minutesField;
+	private ImprovedFormattedTextField secondsField;
 
 	public EditTimeDialog(CountDownTimer timer) {
 		this.timer = timer;
 
+		NumberFormat integerNumberInstance = NumberFormat.getIntegerInstance();
+        minutesField = new ImprovedFormattedTextField( integerNumberInstance );
+        minutesField.setColumns( 2 );
+        
+        secondsField = new ImprovedFormattedTextField( integerNumberInstance );
+        secondsField.setColumns( 2 );
+        
 		this.init();
 	}
 
@@ -41,7 +53,7 @@ public class EditTimeDialog extends StandardDialog {
 		message.setFont(new Font(message.getFont().getName(), Font.PLAIN, message.getFont().getSize() + 2));
 
 		JPanel panel = new JPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(6, 15, 6, 6));
+		panel.setBorder(BorderFactory.createEmptyBorder(0, 15, 0, 6));
 		panel.setLayout(new MigLayout());
 		panel.add(message, "wrap");
 		return panel;
@@ -50,20 +62,29 @@ public class EditTimeDialog extends StandardDialog {
 	@Override
 	public ButtonPanel createButtonPanel() {
 		ButtonPanel panel = new ButtonPanel();
-		panel.setBorder(BorderFactory.createEmptyBorder(10, 15, 6, 6));
+		panel.setBorder(BorderFactory.createEmptyBorder(0, 6, 0, 6));
+		panel.setLayout(new MigLayout("fillx"));
 
-		SetTimeAction setTimeAction = new SetTimeAction(this.timer, this, this.minutesField, this.secondsField);
+		JButton cancelButton = new JButton();
 		CancelButtonAction cancelAction = new CancelButtonAction(this);
-
-		JButton button = new JButton();
-		button.setAction(setTimeAction);
-		this.setDefaultAction(setTimeAction);
-		panel.add(button);
-
-		button = new JButton();
-		button.setAction(cancelAction);
 		this.setDefaultCancelAction(cancelAction);
-		panel.add(button);
+		cancelButton.setAction(cancelAction);
+
+		ImageIcon cancelIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource(IconFactory.INCORRECT));
+		cancelButton.setIcon(cancelIcon);
+
+		panel.add(cancelButton, "w 150!, h 75!");
+
+		JButton okButton = new JButton();
+		AbstractAction okAction = new SetTimeAction(this.timer, this, this.minutesField, this.secondsField);
+		okButton.setAction(okAction);
+		this.setDefaultAction(okAction);
+		okButton.requestFocus();
+
+		ImageIcon okIcon = new ImageIcon(ClassLoader.getSystemClassLoader().getResource(IconFactory.CORRECT));
+		okButton.setIcon(okIcon);
+
+		panel.add(okButton, "gapleft 10, w 150!, h 75!");
 
 		return panel;
 	}
@@ -73,11 +94,11 @@ public class EditTimeDialog extends StandardDialog {
 		final JPanel panel = new JPanel();
 		final JLabel colonLabel = new JLabel(" : ");
 
-		panel.setLayout(new MigLayout("wrap 3, insets 0, fill"));
-		panel.setBorder(BorderFactory.createEmptyBorder(0, 15, 6, 15));
-		panel.add(this.minutesField, "alignx center, growx, sizegroupx 1, pushx");
-		panel.add(colonLabel);
-		panel.add(this.secondsField, "alignx center, growx, sizegroupx 1, pushx");
+		panel.setLayout(new MigLayout("fill"));
+		panel.setBorder(BorderFactory.createEmptyBorder(0, 15, 0, 15));
+		panel.add(this.minutesField, "align right, w 60!, h 45!");
+		panel.add(colonLabel, "align center");
+		panel.add(this.secondsField, "align left, w 60!, h 45!");
 		return panel;
 	}
 }
